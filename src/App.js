487d54b12692bfd1d3c3d00879856bf492e4aaf2
SPX <87719485+Aislots@users.noreply.github.com>
2022-01-27 09:41:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+    liff.init({ liffId: '1656823949-AMDWL1oN' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
